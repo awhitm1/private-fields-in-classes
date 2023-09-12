bb5bfab52744495959d6a864b08bea8c5c3b49e8
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2023-09-11 21:22:40 -0500

    9-11-23 Exercise 6

--- person.js
+++ person.js
@@ -1,5 +1,14 @@
 class Person {
   // Write code here
+  #id;
+  constructor (){
+    this.#id=Math.ceil(Math.random()*(10**3));
+  }
+  get getID(){
+    return this.#id;
+  }
+
 }
+// Score! It works!
 
 module.exports = Person;
